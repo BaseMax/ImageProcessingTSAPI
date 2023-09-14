aba92d13f366baef091db195997781167a3cc169
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-14 23:34:26 +0330

    End detect

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,10 @@
-import * as tf from "@tensorflow/tfjs";
-import "@tensorflow/tfjs-node-gpu";
 const canvas = require('canvas');
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import path from 'path';
 import multer from 'multer';
+import fs from 'fs';
 import * as faceapi from 'face-api.js';
 
 
@@ -13,7 +12,6 @@ const minConfidence = 0.5;
 const faceDetectionOptions = new faceapi.SsdMobilenetv1Options({
   minConfidence
 });
-
 
 const { Canvas , Image , ImageData } = canvas;
 faceapi.env.monkeyPatch({Canvas , Image , ImageData})
@@ -24,10 +22,14 @@ const port = process.env.PORT || 3001;
 
 
 (async()=>{
-  await faceapi.nets.tinyFaceDetector.loadFromDisk(path.join(__dirname , '../models'));
-  await faceapi.nets.faceLandmark68TinyNet.loadFromDisk(path.join(__dirname , '../models'));
-  await faceapi.nets.faceRecognitionNet.loadFromDisk(path.join(__dirname , '../models'));
-  await faceapi.nets.ssdMobilenetv1.loadFromDisk(path.join(__dirname , '../models'));
+  const modelPath = path.join(__dirname , '../models')
+
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath);
+  await faceapi.nets.tinyFaceDetector.loadFromDisk(modelPath);
+  await faceapi.nets.faceLandmark68TinyNet.loadFromDisk(modelPath);
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath)
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath);
+  await faceapi.nets.faceExpressionNet.loadFromDisk(modelPath);
 })()
 
 
@@ -35,7 +37,6 @@ const storage = multer.diskStorage({
   destination: 'public/',
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    // Change the filename to something unique, for example, add a timestamp
     const newFileName = uniqueSuffix + path.extname(file.originalname);
     cb(null, newFileName);
   },
@@ -55,11 +56,23 @@ app.post('/detect', upload.single('image'), async (req, res) => {
   const image = await canvas.loadImage(file)
   
   try {
-    const detections = await faceapi.detectAllFaces(image, faceDetectionOptions)
+    const detections = await faceapi.detectSingleFace(image, faceDetectionOptions).withFaceLandmarks().withFaceExpressions()
 
-  
+    const output = faceapi.createCanvasFromMedia(image);
+    faceapi.draw.drawDetections(output , detections.detection);
+    faceapi.draw.drawFaceLandmarks(output , detections.landmarks);
+    faceapi.draw.drawFaceExpressions(output , detections);
 
-    res.send(detections)
+    console.log(output) ;
+    const result = (output as any).toBuffer('image/jpeg')
+
+
+    await fs.writeFileSync(
+      path.join(__dirname, '../', `public/output/output_${Date.now()}.jpg`),
+      result
+    );
+
+    res.send('ok')
   } catch (error) {
     res.send(error)
   }
