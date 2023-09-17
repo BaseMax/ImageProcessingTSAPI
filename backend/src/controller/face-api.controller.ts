8e1178e5fbc43594521ddfd632cfc27776f2b587
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-17 22:45:11 +0330

    Use detect for age

--- backend/src/controller/face-api.controller.ts
+++ backend/src/controller/face-api.controller.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { Request, Response, Router } from "express";
 import * as faceapi from 'face-api.js';
+import sizeOf, { imageSize } from 'image-size';
 
 
 const minConfidence = 0.5;
@@ -13,32 +14,41 @@ const faceDetectionOptions = new faceapi.SsdMobilenetv1Options({
 const { Canvas , Image , ImageData } = canvas;
 faceapi.env.monkeyPatch({Canvas , Image , ImageData});
 
+const modelPath = path.join(__dirname , '../../models')
 
-(async()=>{
-    const modelPath = path.join(__dirname , '../../models')
-  
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath);
-    await faceapi.nets.tinyFaceDetector.loadFromDisk(modelPath);
-    await faceapi.nets.faceLandmark68TinyNet.loadFromDisk(modelPath);
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath)
-    await faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath);
-    await faceapi.nets.faceExpressionNet.loadFromDisk(modelPath);
-})()
-
+Promise.all([
+   faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath),
+   faceapi.nets.ageGenderNet.loadFromDisk(modelPath),
+   faceapi.nets.tinyFaceDetector.loadFromDisk(modelPath),
+   faceapi.nets.faceLandmark68TinyNet.loadFromDisk(modelPath),
+   faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath),
+   faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath),
+   faceapi.nets.faceExpressionNet.loadFromDisk(modelPath)
+]).then()
 
 const detect =async (req:Request , res:Response) => {
     const file = req.file.path
-  
+    const imageSize = sizeOf(file);  
+    const resize =  {width : imageSize.width , height :imageSize.height};
     const image = await canvas.loadImage(file)
-    
     try {
-      const detections = await faceapi.detectAllFaces(image, faceDetectionOptions).withFaceLandmarks().withFaceExpressions()
-  
+      const detections = await faceapi.detectAllFaces(image, faceDetectionOptions)
+        .withFaceLandmarks()
+        .withFaceExpressions()
+        .withAgeAndGender()
+        
+      const resizedDetections = faceapi.resizeResults(detections ,resize)
       const output = faceapi.createCanvasFromMedia(image);
+      
       faceapi.draw.drawDetections(output , detections);
       faceapi.draw.drawFaceLandmarks(output , detections);
       faceapi.draw.drawFaceExpressions(output , detections);
-  
+      resizedDetections.forEach( detection => {
+        const box = detection.detection.box
+        const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+        drawBox.draw(output)
+      })
+      
       const result = (output as any).toBuffer('image/jpeg')
   
   
