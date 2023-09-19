fe2147225483d5dc1a7c72fc34a3596b4ec430cb
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-19 17:38:07 +0330

    Add auth middleware

--- src/router/face-api.ts
+++ src/router/face-api.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { detect } from "../controller/face-api.controller";
 import { upload } from "../multer/upload";
+import passport from "passport";
 
 const router = Router();
 
+router.use(passport.authenticate('jwt' , {session:false}));
 router.post('/detect', upload.single('image') , detect);
 
 
