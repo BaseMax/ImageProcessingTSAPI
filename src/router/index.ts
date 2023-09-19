db24958f8ea7dc62b7075555f146c19c3a8df44d
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-19 17:38:35 +0330

    Add user router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import faceApiRouter from './face-api';
+import authRouter from './auth';
+import userRouter from './user';
+
 const router = Router();
 
-router.use('/' , faceApiRouter);
+router.use('/face' , faceApiRouter);
+router.use('/auth' , authRouter);
+router.use('/user' , userRouter);
 
 export default router ;
