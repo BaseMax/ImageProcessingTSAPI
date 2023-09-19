a263ff72cc7ff95c7c6b1a08540f2dcc9389b675
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-19 17:25:30 +0330

    add passport middleware

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,38 @@
+import { config } from 'dotenv';
+config()
+
+import './passport/passport.strategy';
+
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import http from 'http';
-import { Server } from 'socket.io';
 import mianRouter from './router/index';
+import { databaseConnection } from './config/database.config';
+import passport from 'passport';
 
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 3001;
-const io = new Server(server , {cors : {
-  origin: 'http://localhost:3000', 
-  methods: ['GET', 'POST'],
-}});
 
-
-
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('data', (data) => {  
-    socket.broadcast.emit('data', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 app.use(morgan('tiny'));
 app.use(helmet());
 app.use(cors());
+app.use(passport.initialize())
 app.use(mianRouter);
 
 
-
-server.listen(port , ()=>{
-    console.log(process.version);
-    console.log(`App runing on port ${port}`)
+server.listen(port , async ()=>{
+  console.log(process.version);
+  await databaseConnection()
+  console.log(`App runing on port ${port}`)
 })
+
 
 export {server}
